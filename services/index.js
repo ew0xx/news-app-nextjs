50c075f815994da90b890e516f84bf57e51d5ac3
ew0xx <96946710+ew0xx@users.noreply.github.com>
2022-12-20 20:39:28 +0300

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,6 +1,6 @@
 import { request, gql } from "graphql-request";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:1337/graphql"
+const graphqlAPI = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://89.116.228.21:1337/graphql"
 export const getCategories = async () => {
   const query = gql`
     query GetCategories {
